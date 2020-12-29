479b03ee43d010213ea1d45468c31de5b4a5a0c2
Mattias <mattias.riedl@stud.fh-campuswien.ac.at>
2020-12-29 19:51:56 +0100

    simplify code in Class "Setting"

--- src/main/java/Setting.java
+++ src/main/java/Setting.java
@@ -5,8 +5,12 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 import java.util.Arrays;
 
 
@@ -44,29 +48,49 @@ public class Setting extends Application {
         leftMainBoxUpperHalf.getChildren().add(rightMainBoxVertical);
 
         //Positionen, damit der text an der richtigen Stelle angezeigt wird. muss für jede Box extra gesetzt werden.
-        leftMainBoxUpperHalf.setAlignment(Pos.CENTER);
+        for(HBox x: Arrays.asList(leftMainBoxLowerHalf,leftMainBoxUpperHalf,ball,tempo,infiz,regeneration)){
+            x.setAlignment(Pos.CENTER);
+        }
         rightMainBoxVertical.setAlignment(Pos.TOP_CENTER);
+            /*
+        leftMainBoxUpperHalf.setAlignment(Pos.CENTER);
+
+             */
+
+        /*
         ball.setAlignment(Pos.TOP_CENTER);
         tempo.setAlignment(Pos.TOP_CENTER);
         infiz.setAlignment(Pos.TOP_CENTER);
         regeneration.setAlignment(Pos.TOP_CENTER);
         leftMainBoxLowerHalf.setAlignment(Pos.CENTER);
 
+         */
+
         //Label - der ganze Label Text
         Label settings = new Label("Settings");
-        Label numberOfBalls = new Label("Anzahl der Bälle");
+        Label numberOfBalls = new Label("Anzahl der Baelle");
         Label geschwindigkeit = new Label("Geschwindigkeit");
         Label infiziert = new Label("Chance zum infizieren");
         Label heilung = new Label("Heilung");
 
         //Font size - der ganze Label Text wird größer und fett geschrieben, für die lesbarkeit
         settings.setStyle("-fx-font-size: 20;"+"-fx-font-weight: bold");
+        for(Label x: Arrays.asList(numberOfBalls,geschwindigkeit,infiziert,heilung)){
+            x.setStyle("-fx-font-weight: bold");
+        }
+        /*
         numberOfBalls.setStyle("-fx-font-weight: bold");
         geschwindigkeit.setStyle("-fx-font-weight: bold");
         infiziert.setStyle("-fx-font-weight: bold");
         heilung.setStyle("-fx-font-weight: bold");
 
+         */
+
         //In der rechten oberen hälfte werden alle Labels und radio buttons eingefügt
+        for(Region x: Arrays.asList(settings,numberOfBalls,ball,geschwindigkeit,tempo,infiziert,infiz,heilung,regeneration)){
+            rightMainBoxVertical.getChildren().add(x);
+        }
+        /*
         rightMainBoxVertical.getChildren().add(settings);
         rightMainBoxVertical.getChildren().add(numberOfBalls);
         rightMainBoxVertical.getChildren().add(ball);
@@ -78,12 +102,15 @@ public class Setting extends Application {
         rightMainBoxVertical.getChildren().add(regeneration);
 
 
+         */
+
         //--------------------Radio Button werden erstellt----------------------------------------------------------------------
         //radio Buttons für Bälle
-        RadioButton einHundert = new RadioButton("100");
-        RadioButton zweiHundert = new RadioButton("200");
-        RadioButton dreiHundert = new RadioButton("300");
-        RadioButton vierHundert = new RadioButton("400");
+
+        RadioButton einHundert = new RadioButton("10");
+        RadioButton zweiHundert = new RadioButton("20");
+        RadioButton dreiHundert = new RadioButton("30");
+        RadioButton vierHundert = new RadioButton("40");
 
         //radio Buttons für Geschwindigkeit
         RadioButton langsam = new RadioButton("Langsam");
@@ -104,25 +131,46 @@ public class Setting extends Application {
         //-------------------Radio buttons werden eingefügt-----------------------------------------------------------------------------
 
         //einfügen bälle
+        for(RadioButton x : Arrays.asList(einHundert,zweiHundert,dreiHundert,vierHundert)){
+            ball.getChildren().add(x);
+        }
+        /*
         ball.getChildren().add(einHundert);
         ball.getChildren().add(zweiHundert);
         ball.getChildren().add(dreiHundert);
         ball.getChildren().add(vierHundert);
-
+         */
         //Einfügen der geschwindigkeit
+        for(RadioButton x : Arrays.asList(langsam,mittel,schnell)){
+            tempo.getChildren().add(x);
+        }
+        /*
         tempo.getChildren().add(langsam);
         tempo.getChildren().add(mittel);
         tempo.getChildren().add(schnell);
 
+         */
+
         //Einfügen der infizierten
+        for(RadioButton x : Arrays.asList(treffer1,treffer2,treffer4)){
+            infiz.getChildren().add(x);
+        }
+        /*
         infiz.getChildren().add(treffer1);
         infiz.getChildren().add(treffer2);
         infiz.getChildren().add(treffer4);
 
+         */
+
         //Einfügen der Heilung
+        for(RadioButton x : Arrays.asList(h30,h60,h120)){
+            regeneration.getChildren().add(x);
+        }
+        /*
         regeneration.getChildren().add(h30);
         regeneration.getChildren().add(h60);
         regeneration.getChildren().add(h120);
+         */
 
         //Gruppen für radio buttons
         ToggleGroup bälle = new ToggleGroup();
@@ -131,25 +179,48 @@ public class Setting extends Application {
         ToggleGroup genesung = new ToggleGroup();
 
         //Radio button groups - bälle
+        for(RadioButton x : Arrays.asList(einHundert,zweiHundert,dreiHundert,vierHundert)){
+            x.setToggleGroup(bälle);
+        }
+        /*
         einHundert.setToggleGroup(bälle);
         zweiHundert.setToggleGroup(bälle);
         dreiHundert.setToggleGroup(bälle);
         vierHundert.setToggleGroup(bälle);
 
+         */
+
         //Radio button groups - geschwind
+        for(RadioButton x : Arrays.asList(langsam,mittel,schnell)){
+            x.setToggleGroup(geschwind);
+        }
+        /*
         langsam.setToggleGroup(geschwind);
         mittel.setToggleGroup(geschwind);
         schnell.setToggleGroup(geschwind);
 
+         */
+
         //Radio button groups - infi
+        for(RadioButton x : Arrays.asList(treffer1,treffer2,treffer4)){
+            x.setToggleGroup(infi);
+        }
+        /*
         treffer1.setToggleGroup(infi);
         treffer2.setToggleGroup(infi);
         treffer4.setToggleGroup(infi);
+         */
 
         //radio button groups - genesung
+        for(RadioButton x : Arrays.asList(h30,h60,h120)){
+            x.setToggleGroup(genesung);
+        }
+        /*
         h30.setToggleGroup(genesung);
         h60.setToggleGroup(genesung);
         h120.setToggleGroup(genesung);
+
+         */
 
         //--------------------------------------------------------------------------------------------------------------
 
@@ -164,15 +235,27 @@ public class Setting extends Application {
         //---------------------------------------------------
 
         //Spacing - abstände zwischen den wörtern und mehr Abstand zu den andern Zeilen
+
+        for(HBox x : Arrays.asList(ball,tempo,infiz,regeneration)){
+            x.setSpacing(10);
+            x.setPadding(new Insets(5,5,15,5));
+        }
+
+         /*
         ball.setSpacing(10);
         tempo.setSpacing(10);
         infiz.setSpacing(10);
         regeneration.setSpacing(10);
+
+          */
         settings.setPadding(new Insets(35,5,15,5));
+        /*
         ball.setPadding(new Insets(5,5,15,5));
         tempo.setPadding(new Insets(5,5,15,5));
         infiz.setPadding(new Insets(5,5,15,5));
         regeneration.setPadding(new Insets(5,5,10,5));
+
+         */
 
         //Border & Farbe
         rightMainBoxVertical.setStyle(
@@ -216,9 +299,15 @@ public class Setting extends Application {
         HBox thirdHBox = new HBox();
 
         //einfügen
+        for(HBox x : Arrays.asList(firstHBox,secondHBox,thirdHBox)){
+            rightMainBox.getChildren().add(x);
+        }
+        /*
         rightMainBox.getChildren().add(firstHBox);
         rightMainBox.getChildren().add(secondHBox);
         rightMainBox.getChildren().add(thirdHBox);
+
+         */
 
         // Button erstellen & hinzufügen
         Button wien = new Button("wien");
@@ -236,6 +325,10 @@ public class Setting extends Application {
         thirdHBox.getChildren().add(platzhalter);
 
         //Button größe umstellen
+        for(Button x : Arrays.asList(wien,haus,venedig,amsterdam,newyork,platzhalter)){
+            x.setMinSize(150,150);
+        }
+        /*
         wien.setMinSize(150,150);
         haus.setMinSize(150,150);
         venedig.setMinSize(150,150);
@@ -243,7 +336,15 @@ public class Setting extends Application {
         newyork.setMinSize(150,150);
         platzhalter.setMinSize(150,150);
 
-        //zentrieren
+         */
+
+        //zentrieren & Styling
+        for(HBox x : Arrays.asList(firstHBox,secondHBox,thirdHBox)){
+            x.setAlignment(Pos.TOP_CENTER);
+            x.setPadding(new Insets(5,5,10,5));
+            x.setSpacing(10);
+        }
+        /*
         firstHBox.setAlignment(Pos.TOP_CENTER);
         secondHBox.setAlignment(Pos.TOP_CENTER);
         thirdHBox.setAlignment(Pos.TOP_CENTER);
@@ -255,6 +356,8 @@ public class Setting extends Application {
         firstHBox.setSpacing(10);
         secondHBox.setSpacing(10);
         thirdHBox.setSpacing(10);
+
+         */
 
         //------------------------------------------------------------------------------------------
         //---------------------------Funktionen-----------------------------------------------------
@@ -318,9 +421,10 @@ public class Setting extends Application {
         */
 //------------------------------------------------------------------------------------------------------------
 
-        /*for(Button x : Arrays.asList(wien,amsterdam,haus,venedig,newyork,platzhalter)){
-            x.setStyle("-fx-border-color: black; " + "-fx-border-width: 2;"+ "-fx-background-image: url('picture/amsterdam.jpg')" );
+        for(Button x : Arrays.asList(wien,amsterdam,haus,venedig,newyork,platzhalter)){
+            x.setStyle("-fx-border-color: black; " + "-fx-border-width: 2;"+ "-fx-background-image: url('picture/amsterdam.jpg');" + "-fx-text-fill: transparent;" );
         }
+        /*
         for(Button x : Arrays.asList(wien,amsterdam,haus,venedig,newyork,platzhalter)){
             if(MainApp.list.get("background").equals(x.getText())){
                 x.setStyle("-fx-border-color: red; " + "-fx-border-width: 2;"+ "-fx-background-image: url('picture/amsterdam.jpg')");
@@ -373,9 +477,9 @@ public class Setting extends Application {
                 @Override
                 public void handle(ActionEvent event) {
                     for(Button x : Arrays.asList(wien,amsterdam,haus,venedig,newyork,platzhalter)){
-                        x.setStyle("-fx-border-color: black; " + "-fx-border-width: 2;"+ "-fx-background-image: url('picture/amsterdam.jpg')" );
+                        x.setStyle("-fx-border-color: black; " + "-fx-border-width: 2;"+ "-fx-background-image: url('picture/amsterdam.jpg');" + "-fx-text-fill: transparent;");
                     }
-                    x.setStyle("-fx-border-color: red; " + "-fx-border-width: 2;"+ "-fx-background-image: url('picture/amsterdam.jpg')" );
+                    x.setStyle("-fx-border-color: red; " + "-fx-border-width: 2;"+ "-fx-background-image: url('picture/amsterdam.jpg');" + "-fx-text-fill: transparent;");
                     /*MainApp.list.put("background",x.getText());*/
                 }
             });
